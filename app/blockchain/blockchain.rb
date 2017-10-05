6f2e5d86ad0fa51116bfa31deee2f8cf4407f6a6
Dmitry Gusev <gusevda90@gmail.com>
2017-10-05 14:28:18 +0300

    new ipc path

--- app/blockchain/blockchain.rb
+++ app/blockchain/blockchain.rb
@@ -5,7 +5,7 @@ class Blockchain
   PROPOSALS_LIMIT = 100
 
   def initialize
-    @client = Ethereum::IpcClient.new('~/.ethereum/testnet/geth.ipc', false)
+    @client = Ethereum::IpcClient.new("#{ENV['HOME']}/Library/Ethereum/testnet/geth.ipc", false)
     @formatter = Ethereum::Formatter.new
 
     contract_json = JSON.parse(File.read('app/blockchain/contracts/charity.json'))
